a77b82e293e9195fe0aa862d1372490ae6bbc62d
Mikita Chyzhyk <mikitachyzhyk@gmail.com>
2021-09-15 14:34:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const loadUsers = async () => {
+    const response = await fetch(
+      'https://itrex-react-lab-files.s3.eu-central-1.amazonaws.com/react-test-api.json'
+    )
+    const users = await response.json()
+    console.log('Users loaded', users)
+    return users
+  }
+
+  const [users, setUsers] = useState({})
+
+  useEffect(() => {
+    setUsers(loadUsers())
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>Search</div>
+        <div>Filter</div>
+      </div>
+
+      <table>
+        <caption>Table Caption</caption>
+      </table>
+
+      <div>pagination</div>
+
+      <div>Info about user</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
